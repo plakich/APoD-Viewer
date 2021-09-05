12563892e5a4d67c24970a6ccf973d23cd1d9280
Phil <lakich@pnw.edu>
2021-09-05 06:29:53 +0000

    initial commit; setup app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  
+  //use later for formatting dates that user chooses
+  let todaysDate = new todaysDate().toLocaletodaysDateString("en-US").split("/").map(el => el.length === 1 ? el.padStart(2,0) : el);
+
+  [todaysDate[1], todaysDate[2], todaysDate[0]] = [todaysDate[0], todaysDate[1], todaysDate[2]];
+  
+  todaysDate = todaysDate.join('-'); //now in proper YYYY-MM-DD format for api
+  
+  let date, desc, title, url, hdurl, thumbnail_url; 
+  
+  fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&thumbs=true')
+    .then(res => res.json())
+    .then(apod => ({date, explanation: desc, title, hdurl, url, thumbnail_url} = apod))
+    .catch(e => console.log(e));
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Intro /> 
+      <Card id={todaysDate} title={title} desc={desc} src={hdurl ? hdurl : (thumbnail_url ? thumbnail_url : url)} />
+      <Footer />
     </div>
   );
 }
