f1d4723bcbb9c9237d732857b1dc789c93fe5649
Phil <lakich@pnw.edu>
2021-09-21 01:08:44 +0000

    change validateDate fn name to validateDateChars and fix logic errors; change setTodayDate fn name to getFormattedDate and change logic to get and format any random date; add shiftMonthDayChars fn to fix final formatting errors for MM/DD formats; add logic to handleChange fn so date is fixed at 10 chars; add logic to handleChange to accommodate one off errors for days (e.g., 31 in month with only 30 days)

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -4,7 +4,7 @@ const DatePicker = () => //will use prop fn from above to handle date selecting
 {
     // const [fromDate, setFromDate] = useState(""); 
     // const [toDate, setToDate] = useState( setDate() ); str.replace(/\//g, '-');
-    const [dateRange, setDateRange] = useState({ fromDate: "", toDate: setTodaysDate() })
+    const [dateRange, setDateRange] = useState({ fromDate: "", toDate: getFormattedDate() });
     
     const handleSubmit = (e) =>
     {
@@ -15,6 +15,7 @@ const DatePicker = () => //will use prop fn from above to handle date selecting
     
     const handleChange = (e) =>
     {
+        const maxDateLength = 10; 
         //1. read string length
         //2. format string based on length
         //3. insert / into correct places
@@ -23,8 +24,16 @@ const DatePicker = () => //will use prop fn from above to handle date selecting
         console.log(e.target.value); 
         
         const name = e.target.name;
-        let value = e.target.value; 
-        let isValidDate = true; 
+        //replace all whitespace because user could have entered YYYY / MM / DD which is 
+        //more than 10 chars (maxDateLength) but that should still be valid. So should
+        //YYYY  MM DD (user entered one too many spaces). 
+        //We also replace hyphens (-) with a forward slash (/) because when new Date is 
+        //given a date with a hyphen, it uses user's local time to set the exact date 
+        //which can result in getting a date one day behind or ahead of the day  
+        //wanted (relative to GMT). 
+        let value = e.target.value.replace(/\s/g, '').replace(/-/g, "/").slice(0, maxDateLength);  
+        let isValidDate = true; //need date in YYYY/MM/DD format. 
+        let errorArray = []; 
         
         //read length
         //format for slashes / if needed (no slashes? insert them, slashes? leave alone)
@@ -35,10 +44,31 @@ const DatePicker = () => //will use prop fn from above to handle date selecting
         
         value = formatSlashes(value);
         
-        isValidDate = validateDate(value); 
+        ({isValidDate, errorArray} = validateDateChars(value)); 
+        
+        if (!isValidDate) 
+        {
+            //try one more time to 
+            //reformat date for month day errors.
+            value = shiftMonthDayChars(value, errorArray);
+            ({isValidDate} = validateDateChars(value));
+            
+        }
         
         if (isValidDate)
         {
+            if (value.length === 10) //a complete date was entered 
+            {
+                
+                //below, getFormattedDate should return the same value as value
+                //(e.g., 1999/09/09 should return 1999/09/09). We try to
+                //format the date again because of edge cases where the
+                //date still wouldn't be correct (e.g., a user who 
+                //accidentally entered 1999/09/31 ignoring that they're aren't
+                //31 days in September). In that case, the date returned will 
+                //be the next valid date (e.g., 1999/10/01); 
+                value = getFormattedDate(value); 
+            }
             setDateRange({...dateRange, [name]: value});
         }
         
@@ -61,22 +91,22 @@ const DatePicker = () => //will use prop fn from above to handle date selecting
     );
 };
 
-const setTodaysDate = () =>
+const getFormattedDate = (date) =>
 {
-    let todaysDate = new Date().toLocaleDateString("en-US").split("/").map(el => el.length === 1 ? el.padStart(2,0) : el);
-    [todaysDate[1], todaysDate[2], todaysDate[0]] = [todaysDate[0], todaysDate[1], todaysDate[2]]; //rearrange in YYYY,MM,DD format
+    let theDate = date ? new Date(date) : new Date(); //get a date or if none provided get todays
     
-    return todaysDate.join("/"); // create string with parts separated by forward slashes YYYY/MM/DD
+    let datePart = theDate.toLocaleDateString("en-US").split("/").map(el => el.length === 1 ? el.padStart(2,0) : el);
+    [datePart[1], datePart[2], datePart[0]] = [datePart[0], datePart[1], datePart[2]]; //rearrange in YYYY,MM,DD format
+    
+    return datePart.join("/"); // create string with parts separated by forward slashes YYYY/MM/DD
 };
 
 //format slashes for dates (e.g., 2001/09/09)
 //by inserting slashes if missing.
 //We don't insert slashes if not needed yet 
-//(e.g., 1999 not 1999/ but need slash for 1999/1 )
+//(e.g., 199 not 199 / but need slash for 1999/ )
 const formatSlashes = (val) => 
 {
-    val = val.replace(/\s/g, ''); //rip out all spaces
-    
     let formattedVal = val; //assume string correctly formatted, below we format if not
     
     const numOfSlashes = val.match(/\//g) ? val.match(/\//g).length : 0;  
@@ -110,24 +140,35 @@ const formatSlashes = (val) =>
     return formattedVal; 
 };
 
-const validateDate = (val) =>
+//fn returns true if all chars of date (e.g., 1999/09/08)
+//are valid (e.g., RRRR/RR/RR is not valid). Does not check if date is actually valid
+//(e.g., 1999/09/31 isn't valid since there aren't 31 days
+//in September)
+const validateDateChars = (val) =>
 {
     
     const charsAllowed = new Map(
         [   [0, "1 2"],
             [1, new Map([
-                            [1, "9"]
+                            [1, "9"] //if 1 was entered as first char of date, only a 9 is valid as 2nd
                         ])],
             [2, new Map([
                             [9, "9"]
                         ])],
             [3, new Map([
-                            [9, "5"]
+                            [9, "5 6 7 8 9"]
                         ])],
             [4, "/"],
             [5, "0 1"],
+            [6, new Map([
+                            [0, "1 2 3 4 5 6 7 8 9"]
+                        ])],
             [7, "/"],
-            [8, "0 1 2 3"]
+            [8, "0 1 2 3"],
+            [9, new Map([
+                            [0, '1 2 3 4 5 6 7 8 9'],
+                            [3, "0 1"]
+                        ])]
     ]);
     
     let formattedInput = val.split("");
@@ -135,32 +176,52 @@ const validateDate = (val) =>
     formattedInput = formattedInput.map((dateChar, i) =>
     {
         let isValidChar = true; 
-        isValidChar = charsAllowed.get(i) ? 
-                                              ( typeof charsAllowed.get(i) === "string" 
-                                              ? charsAllowed.get(i).indexOf(dateChar) !== -1 : charsAllowed.get(i).get( Number( formattedInput[i-1] ) ) ) 
+        isValidChar = charsAllowed.get(i) ? ( typeof charsAllowed.get(i) === "string" 
+                                              ? charsAllowed.get(i).indexOf(dateChar) !== -1
+                                              : ( charsAllowed.get(i).get( Number( formattedInput[i-1] ) ) 
+                                                  ? charsAllowed.get(i).get( Number( formattedInput[i-1] ) ).indexOf(dateChar) !== -1 
+                                                  : Number.isInteger(Number(dateChar)) ) )
                                           : Number.isInteger(Number(dateChar)); 
-        return isValidChar ? dateChar : "E"; //E for error 
+       
+        return isValidChar && i < 10 ? dateChar : "E"; //E is for error. Every char that makes string over 10 chars long is invalid since date is YYYY/MM/DD format
     });
+    
     
     //classList.add(error) for each date part that has error
    
-    return formattedInput.indexOf("E") === -1; 
+    return {isValidDate: formattedInput.indexOf("E") === -1, errorArray: formattedInput}; 
     
-    
-    // const currentChar = val.charAt(val.length - 1); 
-    // const isInt = Number.isInteger(val);
-    
-    // if(!isInt)
-    // {
-    //     return val.slice(0, val.length -1); 
-    // }
-    
-    // if(val.length > 0 && val.length < 2)
-    // {
-        
-    // }
-    
-}
+};
 
+const shiftMonthDayChars = (value, errorArray) =>
+{
+    const monthStartIndex = 5; 
+    let arrValue = value.split(""); 
+    
+    if (errorArray.indexOf("E") !== -1 && errorArray.lastIndexOf("E") >= monthStartIndex) //look for errors in month and day (MM/DD)
+    {
+        const maxDateLength = 10; 
+        
+        for (let i = monthStartIndex; i < errorArray.length; i++) 
+        {
+            if (errorArray[i] === "E" && Number.isInteger( Number( arrValue[i] ) ))
+            {
+                //see if we can insert zeros in error positions to make valid date (e.g. 1999/5/5 -> 1999/05/05
+                //or 1999/9999 -> 1999/09/09)
+                 arrValue[i] = "0" + arrValue[i]; 
+                 value = formatSlashes(arrValue.join("")); 
+                 ({errorArray} = validateDateChars(value));
+                 arrValue = value.split("").slice(0, maxDateLength); 
+                 
+            }
+            
+        }
+        
+        value = arrValue.join(""); //reform arr as string
+    }
+
+    return value;
+    
+};
 
 export default DatePicker; 
