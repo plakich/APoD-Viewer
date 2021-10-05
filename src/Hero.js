2bd6d0706bd80e11743f3877b27bb0a08cd1b959
Phil <lakich@pnw.edu>
2021-10-05 03:05:21 +0000

    create conditionally rendered header (fixed vs static); place another DatePicker in header; start styles for mobile.
    I added scroll and resize listeners that
    handle adding the fixed header based on
    if the user is below the date range
    selector at the bottom of Hero section.
    I also added another DatePicker component
    to the fixed header so the user can select
    a new date range no matter where they're at
    on the screen. I also added a nice little
    fade in animation so the header doesn't just
    pop in.

--- src/Hero.js
+++ src/Hero.js
@@ -6,6 +6,7 @@ const Hero = ({setDateRange, apod}) =>
 {
     
     const [isApodSet, setIsApodSet] = useState(apod);
+    const [isScrolled, setIsScrolled] = useState(false); 
     
     useEffect(() =>
     {
@@ -22,23 +23,57 @@ const Hero = ({setDateRange, apod}) =>
         }
         
     }, [apod]);
-              
+    
+    useEffect(() =>
+    {
+        const handleScroll = (e) =>
+        {
+            const datePicker = document.querySelector(".hero__title + .form-container"); 
+            
+            const datePickerBtm = datePicker.getBoundingClientRect().bottom; 
+            
+            if ( datePickerBtm <= 0 )
+            {
+                setIsScrolled(true);    
+                datePicker.querySelector("button").disabled = true;
+                
+            }
+            else
+            {
+                setIsScrolled(false);
+                datePicker.querySelector("button").disabled = false;
+            }
+            
+        };
+        
+        window.addEventListener("scroll", handleScroll); 
+        window.addEventListener("resize", handleScroll); 
+        
+    }, []);
+        
     return  (
     
         <section className="hero">
-            <header>
-                <Logo className="logo"/>
-                <h3 className="logo-text">APoD Viewer</h3>
-            </header>
+            {
+                isScrolled ? 
+                    <header className="header-fixed">
+                        <Logo className="logo"/>
+                        <h3 className="logo-text">APoD Viewer</h3>
+                        <DatePicker setDateRange={setDateRange}/>
+                    </header>
+                            : 
+                    <header>
+                        <Logo className="logo"/>
+                        <h3 className="logo-text">APoD Viewer</h3>
+                    </header>
+            }
             <div className="hero__title">
                 <h1 className="hero__title__text">
                     Astronomy Picture of the Day Viewer
                 </h1>
             </div>
             <DatePicker setDateRange={setDateRange}/>
-           
-           
-            
+               
         </section>
     );
 };
